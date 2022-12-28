61d5453e6604a3e0c5965840fa10897ae3a5e473
leonardo-chavesta <chavestatorresleonardo@gmail.com>
2022-12-28 09:20:12 -0500

    Add model Editorial

---
+++ Domain/Editorial.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Editorial
+    {
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public string  Nombre { get; set; }
+        public DateTime FechaRegistro { get; set; } = DateTime.UtcNow;
+        public int Estado { get; set; } = 1;
+
+    }
+}
