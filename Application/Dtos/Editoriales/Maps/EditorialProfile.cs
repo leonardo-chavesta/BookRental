c8bf3872aaa3a2e3c97df75f34b435bc954d7997
leonardo-chavesta <chavestatorresleonardo@gmail.com>
2022-12-28 11:00:22 -0500

    añadiendo editoria

---
+++ Application/Dtos/Editoriales/Maps/EditorialProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Domain;
+
+namespace Application.Dtos.Editoriales.Maps
+{
+    public class EditorialProfile : Profile
+    {
+        public EditorialProfile() 
+        {
+            CreateMap<Editorial, EditorialDto>();
+        }   
+    }
+}
---
+++ Application/Services/Implementations/EditorialService.cs
@@ -0,0 +1,30 @@
+﻿using Application.Dtos.Editoriales;
+using Application.Services.Abstractions;
+using AutoMapper;
+using Domain;
+using Infrastructure.Repositories.Abstractions;
+
+namespace Application.Services.Implementations
+{
+    public class EditorialService : IEditorialService
+    {
+        private readonly IMapper _mapper;
+        private readonly IEditorialRepository _editorialRepository;
+
+        public EditorialService(IMapper mapper ,IEditorialRepository editorialRepository) 
+        {
+            _mapper = mapper;
+            _editorialRepository = editorialRepository;
+        }
+
+        public async Task<IList<EditorialDto>> FindAll()
+        {   
+            // Con List 
+            //IList<Editorial> editorials = await _editorialRepository.findAll();
+            //return _mapper.Map<IList<EditorialDto>>(editorials);
+             
+           var responser = await _editorialRepository.FindAll();
+           return _mapper.Map<IList<EditorialDto>>(responser);
+        }
+    }
+}
