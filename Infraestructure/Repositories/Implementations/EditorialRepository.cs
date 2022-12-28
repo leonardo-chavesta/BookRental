7e7c03b9adb437c5203104211101b5cb2bc43466
leonardo-chavesta <chavestatorresleonardo@gmail.com>
2022-12-28 10:28:35 -0500

    Editorial Repositore - Method FinAlld

---
+++ Infraestructure/Repositories/Implementations/EditorialRepository.cs
@@ -0,0 +1,38 @@
+﻿using Domain;
+using Infrastructure.Context;
+using Infrastructure.Repositories.Abstractions;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories.Implementations
+{
+    public class EditorialRepository : IEditorialRepository
+    {
+        private readonly ApplicationDbContext _context;
+        
+
+        public EditorialRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        //public async Task<IList<Editorial>> findAll()
+        //{
+        //    var responce = await _context.Editoriales.ToListAsync();
+
+        //    return responce;
+
+        //}
+        /* con funcion flecha y ordendo que el ultimo regitro se muestre primero */
+
+        public async Task<IList<Editorial>> findAll() => await _context.Editoriales.OrderByDescending(e => e.Id).ToListAsync();
+       
+
+
+
+    }
+}
